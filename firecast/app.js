35662bad93509bf3954d5af9d7a4ebc9293e58f9
felipejsm <felipejsm41@gmail.com>
2017-09-11 07:01:37 -0300

    Firebase js config file

---
+++ firecast/app.js
@@ -0,0 +1,11 @@
+(function() {
+    const config = {
+        apiKey: "AIzaSyB4ChqpIS8fUANIg2684bncfVXiWE5zMNk",
+        authDomain: "web-quickstart-dc5d8.firebaseapp.com",
+        databaseURL: "https://web-quickstart-dc5d8.firebaseio.com",
+        projectId: "web-quickstart-dc5d8",
+        storageBucket: "web-quickstart-dc5d8.appspot.com",
+        messagingSenderId: "192940399504"
+    };
+    firebase.initializeApp(config);
+}());
